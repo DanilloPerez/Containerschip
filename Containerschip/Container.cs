5601998c698485933b19b31d58692c0c2baecde4
Danillo Perez <danillo0101@live.nl>
2020-10-31 23:01:05 +0100

    first commit

---
+++ Containerschip/Container.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Containerschip
+{
+    public class Container
+    {
+        public enum containerType
+        {
+            Cooled,
+            Regular,
+            Valueable
+        }
+        public enum ContainerWeight
+        {
+            full = 30000,
+            empty = 4000
+        }
+        public containerType containertype { get; set; }
+        public int containerweight { get; set; }
+
+        public Container(containerType type, int weight)
+        {
+            this.containertype = type;
+            this.containerweight = weight;
+        }
+    }
+}
---
+++ Containerschip/schip.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Containerschip
+{
+    class schip
+    {
+        public enum ShipSize
+        {
+            Height,
+            Width
+        }
+
+    }
+}
